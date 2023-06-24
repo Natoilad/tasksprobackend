5cd218f06cf132afe056ad73f63e31547e98524c
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-06-24 18:26:23 +0300

    server, swagger and cloudinary

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const { DB_HOST } = process.env;
-// mongoose.set('strictQuery', true);
+const { DB_HOST, PORT, BASE_URL } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log('Database connection successful');
+    app.listen(PORT, () => {
+      console.log(`Database connect success, ${BASE_URL}`);
     });
   })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
+
