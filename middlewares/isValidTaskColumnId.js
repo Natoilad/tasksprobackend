0c326a0926f87aecf5c89cf1b2c925a179f26876
Yulia Kutova <kutova85@gmail.com>
2023-07-02 21:37:36 +0300

    add

--- middlewares/isValidTaskColumnId.js
+++ middlewares/isValidTaskColumnId.js
@@ -1,13 +1,13 @@
-
 const { isValidObjectId } = require("mongoose");
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-    const { columnId } = req.params;
-    if (!isValidObjectId(columnId)) {
-        next(HttpError(400, `${contactId} is not valid id`))
-    }
-    next();
-}
+  const { columnId } = req.params;
+  if (!isValidObjectId(columnId)) {
+    // next(HttpError(400, `${contactId} is not valid id`))
+    next(HttpError(400, `${columnId} is not valid id`));
+  }
+  next();
+};
 
 module.exports = isValidId;
