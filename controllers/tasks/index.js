b53f9280b35c86fbf0e5fd7531cc4561e4917219
Maksym235 <andriichukwork235@gmail.com>
2023-07-03 20:47:45 +0300

    adds  tasks route

--- controllers/tasks/index.js
+++ controllers/tasks/index.js
@@ -1,17 +1,18 @@
 const { ctrlWrapper } = require("../../helpers");
-const addTask = require('./addTask');
-const listTasks = require('./listTasks');
-const getTaskById = require('./getTaskById');
-const removeTask = require('./removeTask');
-const updateTask = require('./updateTask');
-const getTaskByColumnId = require('./getTaskByColumnId');
-
+const addTask = require("./addTask");
+const listTasks = require("./listTasks");
+const getTaskById = require("./getTaskById");
+const removeTask = require("./removeTask");
+const updateTask = require("./updateTask");
+const getTaskByColumnId = require("./getTaskByColumnId");
+const transferTasks = require("./transferTasks");
 
 module.exports = {
-    addTask: ctrlWrapper(addTask),
-    listTasks: ctrlWrapper(listTasks),
-    getTaskById: ctrlWrapper(getTaskById),
-    removeTask: ctrlWrapper(removeTask),
-    updateTask: ctrlWrapper(updateTask),
-    getTaskByColumnId: ctrlWrapper(getTaskByColumnId),
-}
+  addTask: ctrlWrapper(addTask),
+  listTasks: ctrlWrapper(listTasks),
+  getTaskById: ctrlWrapper(getTaskById),
+  removeTask: ctrlWrapper(removeTask),
+  updateTask: ctrlWrapper(updateTask),
+  getTaskByColumnId: ctrlWrapper(getTaskByColumnId),
+  transferTasks: ctrlWrapper(transferTasks),
+};
--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,17 +1,47 @@
-const express = require('express')
-const { addTask, listTasks, getTaskById, removeTask, updateTask, getTaskByColumnId } = require("../../controllers/tasks");
+const express = require("express");
+const {
+  addTask,
+  listTasks,
+  getTaskById,
+  removeTask,
+  updateTask,
+  getTaskByColumnId,
+  transferTasks,
+} = require("../../controllers/tasks");
 const { addTaskSchema, updateSchema } = require("../../models");
-const { isValidTaskId, authenticate, isValidTaskColumnId } = require("../../middlewares");
+const {
+  isValidTaskId,
+  authenticate,
+  isValidTaskColumnId,
+} = require("../../middlewares");
 const { validBody } = require("../../middlewares/index");
+const { transferTaskSchema } = require("../../models/tasks");
 
 const tasksRouter = express.Router();
 
 tasksRouter.get("/", authenticate, listTasks);
 tasksRouter.post("/", authenticate, validBody(addTaskSchema), addTask);
 tasksRouter.get("/:taskId", authenticate, isValidTaskId, getTaskById);
-tasksRouter.get("/column/:columnId", authenticate, isValidTaskColumnId, getTaskByColumnId);
-tasksRouter.put("/:taskId", authenticate, isValidTaskId, validBody(updateSchema), updateTask);
+tasksRouter.get(
+  "/column/:columnId",
+  authenticate,
+  isValidTaskColumnId,
+  getTaskByColumnId
+);
+tasksRouter.put(
+  "/:taskId",
+  authenticate,
+  isValidTaskId,
+  validBody(updateSchema),
+  updateTask
+);
+tasksRouter.patch(
+  "/:taskId",
+  authenticate,
+  isValidTaskId,
+  validBody(transferTaskSchema),
+  transferTasks
+);
 tasksRouter.delete("/:taskId", authenticate, isValidTaskId, removeTask);
 
-
-module.exports = tasksRouter
+module.exports = tasksRouter;
